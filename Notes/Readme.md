08b81fe4a22174574f758418ce7203dfa37953a0
AleSergio <46831993+AleSergio@users.noreply.github.com>
2020-08-09 13:35:30 +0300

    Note add

---
+++ Notes/Readme.md
@@ -0,0 +1,13 @@
+# Notes
+
+Simple note writer, that is used to write notes in 3 categories. All data saves in files and you can reopen app with saved data.
+
+![imgMain](images/image1.jpg)
+
+You can choose categories by taping on them and Alpha is being changed, giving the icons fade in and fade out event.
+
+![imgMain](images/image2.jpg)
+
+Because this note writer has only basic activities, you can only clear all data and you are not able to edit data inside.
+
+![imgMain](images/image3.jpg)
---
+++ Notes/src/main/java/com/example/textview/MainActivity.java
@@ -0,0 +1,83 @@
+package com.example.textview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(readFromFile(this));
+    }
+
+    private void writeToFile(String data,Context context) {
+        try {
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(context.openFileOutput("new.txt", Context.MODE_PRIVATE));
+            outputStreamWriter.write(data);
+            outputStreamWriter.close();
+        }
+        catch (IOException e) {
+            Log.e("Exception", "File write failed: " + e.toString());
+        }
+    }
+
+    private String readFromFile(Context context) {
+
+        String ret = "";
+
+        try {
+            InputStream inputStream = context.openFileInput("new.txt");
+
+            if ( inputStream != null ) {
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                String receiveString = "";
+                StringBuilder stringBuilder = new StringBuilder();
+
+                while ( (receiveString = bufferedReader.readLine()) != null ) {
+                    stringBuilder.append("\n").append(receiveString);
+                }
+
+                inputStream.close();
+                ret = stringBuilder.toString();
+            }
+        }
+        catch (FileNotFoundException e) {
+            Log.e("login activity", "File not found: " + e.toString());
+        } catch (IOException e) {
+            Log.e("login activity", "Can not read file: " + e.toString());
+        }
+
+        return ret;
+    }
+
+    protected void onClick(View v) {
+        EditText editText = findViewById(R.id.editText);
+        StringBuilder builder = new StringBuilder();
+        builder.append(editText.getText());
+        String text = builder.toString();
+        writeToFile(text, this);
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(readFromFile(this));
+    }
+}
