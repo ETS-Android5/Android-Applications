1f45b1e20c97a22b2d6ae83d9785e140b60f9aa4
Sergei Aleksejev <46831993+AleSergio@users.noreply.github.com>
2022-05-24 14:51:18 +0300

    Update CreateList.java

--- SmartGallery/src/main/java/com/example/smallgallery/CreateList.java
+++ SmartGallery/src/main/java/com/example/smallgallery/CreateList.java
@@ -1,14 +1,18 @@
-package com.example.smallgallery;
+public objectDetection(String foundObject) throws IOException, ModelException, TranslateException {
+    BufferedImage img = BufferedImageUtils.fromVideoStream(foundObject);
 
-public class CreateList {
+    Criteria<BufferedImage, DetectedObjects> criteria =
+            Criteria.builder()
+                    .optApplication(Application.CV.OBJECT_DETECTION)
+                    .setTypes(BufferedImage.class, DetectedObjects.class)
+                    .optFilter("backbone", "resnet50")
+                    .optProgress(new ProgressBar())
+                    .build();
 
-    private Integer image_id;
-
-    public Integer getImage_ID() {
-        return image_id;
-    }
-
-    public void setImage_ID(Integer android_image_url) {
-        this.image_id = android_image_url;
+    try (ZooModel<BufferedImage, DetectedObjects> model = ModelZoo.loadModel(criteria)) {
+        try (Predictor<BufferedImage, DetectedObjects> predictor = model.newPredictor()) {
+            DetectedObjects detection = predictor.predict(img);
+            System.out.println(detection);
+        }
     }
 }
